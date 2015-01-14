519966c6acd523c497184fcc1ae818a67d2f2ca3
brettimus <brbeut@gmail.com>
2015-01-13 16:38:11 -0800

    added js source and code to create a barebones engine

---
+++ lib/stampitjs/rails.rb
@@ -0,0 +1,8 @@
+require "stampitjs/rails/version"
+
+module Stampitjs
+  module Rails
+    class Engine << ::Rails::Engine
+    end
+  end
+end
---
+++ lib/stampitjs/rails/version.rb
@@ -0,0 +1,5 @@
+module Stampitjs
+  module Rails
+    VERSION = "1.1.0"
+  end
+end
